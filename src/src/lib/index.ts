3755998254fb085cba76da32570db88f5837d0ab
MBRoi <support@marketingbleuroi.com>
2026-04-19 02:33:06 +0000

    chore: remove duplicate src/src/ directory

--- src/src/lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { logger } from './logger.js';
-export { cacheGet, cacheSet, hashParams } from './cache.js';
-export { makeMcpError, withMcpMiddleware } from './middleware.js';
-export type { McpErrorCode, McpTextContent, ToolResult, McpErrorResult, ToolCallContext } from './middleware.js';
